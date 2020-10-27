c9e9bf2f96fd7314f7cb23e12dcb7b8fea033e73
Simcha26 <72739040+Simcha26@users.noreply.github.com>
2020-10-27 13:35:29 +0200

    Adding Js_variable exercises

---
+++ Var.js
@@ -0,0 +1,23 @@
+///// Exercise_1
+
+// const maxCapacity = 14
+// let surfTime = true
+// let bestStudent
+// const purposeInLife 
+
+// console.log(maxCapacity)
+// console.log(surfTime)
+// console.log(bestStudent)
+// console.log(purposeInLife)
+
+
+
+///// Exercise_2
+
+// const x = "water"
+// const result = (`${x} is the best thing ever`)
+// console.log(result)
+
+
+///// Exercise_3
+
