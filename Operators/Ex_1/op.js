fb4632f6786cf45479b519d7966d29922f1ba336
Simcha26 <72739040+Simcha26@users.noreply.github.com>
2020-11-10 00:03:48 +0200

    Operators 4 exercises

---
+++ Operators/Ex_1/op.js
@@ -0,0 +1,6 @@
+const password = 123
+const confirmPassword = 123
+
+console.log(password === confirmPassword)
+///// or /////
+console.log(password == confirmPassword)
---
+++ Operators/Ex_3/op.js
@@ -0,0 +1,34 @@
+(5 > 2) && false
+    // False
+
+
+!("knife" === "sword")
+    // True
+
+
+(1 < 2) || (-1 > -1) || !false
+    // True
+
+
+""
+    // False
+
+
+(31 % 5) == "1"
+    // False
+
+
+!!true
+    // True
+
+
+"5th Avenue" != "5th Avenue"
+    // False
+
+
+52 !== "52"
+    // True
+
+
+(undefined || null)
+    // False
